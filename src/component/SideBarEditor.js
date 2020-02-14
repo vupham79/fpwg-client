99b03d252af5106eafbcb69a2005c020e8ed6f58
Vu Pham <vuph79@gmail.com>
2020-02-14 15:09:13 +0700

    Fix: remove redundant code

--- src/component/SideBarEditor.js
+++ src/component/SideBarEditor.js
@@ -1,18 +1,8 @@
 import React from "react";
-import {
-  AppBar,
-  CssBaseline,
-  Drawer,
-  Tabs,
-  Tab,
-  Paper,
-  Grid
-} from "@material-ui/core";
+import { AppBar, CssBaseline, Drawer, Tabs, Tab } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import DesignTab from "./DesignEditorTab";
-import PagesTab from "./PagesEditorTab";
-import SettingTab from "./SettingEditorTab";
 import { updateTabValue } from "../actions";
 import PagesEditorTab from "./PagesEditorTab";
 import SettingEditorTab from "./SettingEditorTab";
@@ -74,22 +64,9 @@ class ClippedDrawer extends React.Component {
             <Tab style={tabStyles} label="Page" />
             <Tab style={tabStyles} label="Setting" />
           </Tabs>
-
-          {tabValue === 0 && (
-            <Grid>
-              <DesignTab />
-            </Grid>
-          )}
-          {tabValue === 1 && (
-            <Grid>
-              <PagesEditorTab />
-            </Grid>
-          )}
-          {tabValue === 2 && (
-            <Grid>
-              <SettingEditorTab />
-            </Grid>
-          )}
+          {tabValue === 0 && <DesignTab />}
+          {tabValue === 1 && <PagesEditorTab />}
+          {tabValue === 2 && <SettingEditorTab />}
         </Drawer>
       </AppBar>
     );
