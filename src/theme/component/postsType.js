8a4039db61cce26ec7d46b0505af774f66a2b7e2
Vu Pham <vuph79@gmail.com>
2020-03-06 16:54:43 +0700

    Fix design theme 2

--- src/theme/component/postsType.js
+++ src/theme/component/postsType.js
@@ -67,7 +67,7 @@ function TypeAlbum({ post, openDialog }) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Grid item xs={8} sm={6} md={3}>
+      <Grid item xs={12} sm={6} md={3}>
         <Card className={classes.card}>
           <CardMedia
             className={classes.cardMediaAlbum}
@@ -115,7 +115,7 @@ function TypePhoto({ post, openDialog }) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Grid item xs={8} sm={6} md={3}>
+      <Grid item xs={12} sm={6} md={3}>
         <Card className={classes.card}>
           <CardMedia
             className={classes.cardMedia}
