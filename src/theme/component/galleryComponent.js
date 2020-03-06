2b2b7b08e2275aa4c24cb48800883bf8138e1b85
superstorm1998 <thinhdq123@gmail.com>
2020-03-06 14:57:05 +0700

    create gallerycomponent, type "album", "photo" for post

---
+++ src/theme/component/galleryComponent.js
@@ -0,0 +1,96 @@
+import {
+  Container,
+  CssBaseline,
+  Dialog,
+  Grid,
+  withStyles,
+  Typography
+} from "@material-ui/core";
+import React from "react";
+import { connect } from "react-redux";
+import { closeDialog, openDialog } from "../../actions";
+
+const useStyles = theme => ({
+  root: {
+    marginTop: theme.spacing(15),
+    marginBottom: theme.spacing(10)
+  },
+  gridItems: {
+    maxHeight: 350
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)"
+  }
+});
+
+const imgStyles = {
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  width: "100%",
+  height: "100%"
+};
+
+class GalleryComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: ""
+    };
+  }
+  render() {
+    const handleOpenDialog = (openDialog, image) => {
+      this.setState({ img: image });
+      openDialog();
+    };
+    const { classes, open, openDialog, closeDialog, galleries } = this.props;
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container className={classes.root}>
+          <Grid container spacing={3} justify="center">
+            {galleries ? (
+              galleries.map((item, index) => (
+                <Grid
+                  item
+                  key={index}
+                  xs={12}
+                  sm={4}
+                  md={2}
+                  className={classes.gridItems}
+                >
+                  <img
+                    src={item.url}
+                    alt="Title"
+                    style={imgStyles}
+                    onClick={() => handleOpenDialog(openDialog, item.url)}
+                    aria-labelledby="form-dialog-title"
+                  />
+                </Grid>
+              ))
+            ) : (
+              <Typography>You don't have any Gallery.</Typography>
+            )}
+          </Grid>
+          <Dialog open={open} onClose={closeDialog} maxWidth="sm" fullWidth>
+            <img style={imgStyles} src={this.state.img} alt="" />
+          </Dialog>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  open: state.dialog.open
+});
+
+const mapDispatchToProps = dispatch => ({
+  openDialog: () => dispatch(openDialog()),
+  closeDialog: () => dispatch(closeDialog())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(useStyles)(GalleryComponent));
---
+++ src/theme/component/postsType.js
@@ -0,0 +1,232 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  CssBaseline,
+  Dialog,
+  Grid,
+  makeStyles,
+  Typography,
+  withStyles,
+  GridListTileBar
+} from "@material-ui/core";
+import React from "react";
+import { connect } from "react-redux";
+
+const useStyles = makeStyles(theme => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8)
+  },
+  card: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+    paddingTop: theme.spacing(3)
+  },
+  readMore: {
+    textDecoration: "underline",
+    "&:hover": {
+      textDecoration: "underline"
+    }
+  },
+  gridItems: {
+    maxHeight: 350
+  }
+}));
+
+const gridStyle = theme => ({
+  root: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(3)
+  },
+
+  images: {
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    width: "-webkit-fill-available",
+    height: "30vh",
+    maxHeight: "100%",
+    maxWidth: "100%"
+  }
+});
+
+function TypeAlbum({ post, openDialog }) {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Grid item xs={8} sm={6} md={3}>
+        <Card className={classes.card}>
+          <GridListTileBar>
+            <CardMedia
+              className={classes.cardMedia}
+              image={post.attachments.images[0]}
+              title="Image title"
+            />
+          </GridListTileBar>
+
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {post.title}
+            </Typography>
+            <Typography>{post.message && post.message}</Typography>
+            <Button
+              color="primary"
+              className={classes.readMore}
+              onClick={() => openDialog(post)}
+            >
+              Read More
+            </Button>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary" variant="contained">
+              View More
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+function TypePhoto({ post, openDialog }) {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Grid item xs={8} sm={6} md={3}>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={post.attachments.images[0]}
+            title="Image title"
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {post.title}
+            </Typography>
+            <Typography>{post.message && post.message}</Typography>
+            <Button
+              color="primary"
+              className={classes.readMore}
+              onClick={() => openDialog(post)}
+            >
+              Read More
+            </Button>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary" variant="contained">
+              View More
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+class PostTypeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      postOpen: null
+    };
+  }
+
+  handleOpen = post => {
+    this.setState({
+      open: true,
+      postOpen: post
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+      postOpen: null
+    });
+  };
+
+  render() {
+    const { posts, classes } = this.props;
+    const post = this.state.postOpen;
+    console.log(post);
+    return (
+      <Container>
+        <Grid
+          container
+          spacing={5}
+          justify="center"
+          style={{ marginTop: "3rem" }}
+        >
+          {posts.map(
+            (post, index) =>
+              (post.attachments.media_type === "photo" && (
+                <TypePhoto
+                  key={index}
+                  post={post}
+                  openDialog={this.handleOpen}
+                />
+              )) ||
+              (post.attachments.media_type === "album" && (
+                <TypeAlbum
+                  key={index}
+                  post={post}
+                  openDialog={this.handleOpen}
+                />
+              ))
+          )}
+          <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            fullWidth
+            maxWidth="md"
+          >
+            <Container className={classes.root}>
+              <Grid container spacing={3} justify="center">
+                {post &&
+                  post.attachments.images.map((img, index) => (
+                    <Grid
+                      item
+                      key={index}
+                      xs={12}
+                      sm={4}
+                      md={4}
+                      className={classes.girdItems}
+                    >
+                      <img src={img} alt="" className={classes.images} />
+                    </Grid>
+                  ))}
+              </Grid>
+              <Grid container className={classes.root} justify="center">
+                <Grid>
+                  <Typography variant="h5" color="textPrimary">
+                    {post && post.title}
+                  </Typography>
+                </Grid>
+                <Grid>
+                  <Typography variant="body1">
+                    {post && post.message}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Container>
+          </Dialog>
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+export default withStyles(gridStyle)(PostTypeComponent);
