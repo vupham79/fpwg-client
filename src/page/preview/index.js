0f5490a412ffcd6c3875a5837ff85cd35dc34e32
(K12 _ HCM) Trinh Tuan Tung <tungttse63174@fpt.edu.vn>
2020-03-12 20:05:27 +0700

    add preview button

---
+++ src/page/preview/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { themes as themesConstant } from "../../constant/constant";
+import { connect } from "react-redux";
+import { Grid } from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import WebFont from "webfontloader";
+class PreViewSite extends React.Component {
+
+  render() {
+    const { siteEdit } = this.props;
+
+    if (siteEdit) {
+
+      WebFont.load({
+        google: {
+          families: [siteEdit.fontTitle, siteEdit.fontBody]
+        }
+      });
+      return themesConstant.find(e => e.id === siteEdit.theme.id).component;
+    }
+    return (
+      <Grid container justify="center">
+        <h1 style={{ color: "red" }}>404 Not Found</h1>
+      </Grid>
+    );
+
+  }
+}
+
+const mapStateToProps = state => ({
+  siteEdit: state.site.siteEdit
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(PreViewSite)
+);
