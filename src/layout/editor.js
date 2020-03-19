be98c670c4c8f7d8f1e9cdd517c094cdbe68dbf0
(K12 _ HCM) Trinh Tuan Tung <tungttse63174@fpt.edu.vn>
2020-03-19 22:48:14 +0700

    preview button setup code for tablet, mobile preview modes

--- src/layout/editor.js
+++ src/layout/editor.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, IconButton } from "@material-ui/core";
 import SiteBarEditor from "../component/SideBarEditor";
-import Header from "../component/Header";
 import { connect } from "react-redux";
+import { setPreviewMode } from "../actions";
+import { faArrowCircleLeft, faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class EditLayout extends Component {
   render() {
@@ -15,14 +17,28 @@ class EditLayout extends Component {
             </Grid>
           )}
           {this.props.isPreview ? (
-            <Grid item sm={12} xs={12}>
-              {this.props.children}
-            </Grid>
-          ) : (
-              <Grid item sm={9} xs={12}>
+            <Grid container item sm={12} xs={12} justify="center" style={{ background: "black" }}>
+              <Grid item xs={12} style={{ background: "white", overflowY: "scroll", overflowX: "hidden", height: "100vh" }}>
                 {this.props.children}
               </Grid>
+            </Grid>
+          ) : (
+              <Grid container item sm={9} xs={12} justify="center" style={{ background: "black" }}>
+                <Grid item xs={12} style={{ background: "white", overflowY: "scroll", overflowX: "hidden", height: "100vh" }}>
+                  {this.props.children}
+                </Grid>
+              </Grid>
             )}
+
+          <IconButton
+            onClick={() => this.props.setPreviewMode(!this.props.isPreview)}
+            style={{ position: "fixed", bottom: 0, zIndex: 9999 }}>
+            <FontAwesomeIcon
+              icon={this.props.isPreview ? faArrowCircleRight : faArrowCircleLeft}
+              color="#0074aa"
+              size="xs"
+            />
+          </IconButton>
         </Grid>
       </>
     );
@@ -33,6 +49,8 @@ const mapStateToProps = state => ({
   isPreview: state.site.isPreview
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  setPreviewMode: bool => dispatch(setPreviewMode(bool))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditLayout);
