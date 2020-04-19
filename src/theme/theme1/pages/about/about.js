3438734de91654e6bc21be090294d2cd81bf1d6d
(K12 _ HCM) Trinh Tuan Tung <tungttse63174@fpt.edu.vn>
2020-04-19 07:09:46 +0700

    theme1 about fix

--- src/theme/theme1/pages/about/about.js
+++ src/theme/theme1/pages/about/about.js
@@ -1,4 +1,4 @@
-import { Grid } from "@material-ui/core";
+import { Grid, lighten } from "@material-ui/core";
 import React from "react";
 import { connect } from "react-redux";
 class Theme1About extends React.Component {
@@ -12,6 +12,29 @@ class Theme1About extends React.Component {
     return siteView.logo;
   };
 
+  getCover = (index) => {
+    const { isEdit, newCover, siteView } = this.props;
+    if (isEdit) {
+      if (newCover && newCover[index]) {
+        if (
+          newCover[index] &&
+          typeof newCover[index] === "object" &&
+          newCover[index].size > 0
+        ) {
+          return URL.createObjectURL(newCover[index]);
+        } else return newCover[index];
+      } else {
+        return "/images/theme1-banner3.jpg";
+      }
+    } else {
+      if (siteView.cover && siteView.cover[index]) {
+        return siteView.cover[index];
+      } else {
+        return "/images/theme1-banner3.jpg";
+      }
+    }
+  };
+
   render() {
     const {
       isEdit,
@@ -23,6 +46,7 @@ class Theme1About extends React.Component {
       bodyView,
       fromHome,
       homeTitle,
+      newCover
     } = this.props;
     const useStyles = () => ({
       changableTitle: {
@@ -34,6 +58,14 @@ class Theme1About extends React.Component {
         paddingBottom: 20,
         textTransform: "uppercase",
       },
+      changableTitle2: {
+        fontFamily: isEdit ? titleEdit.fontFamily : titleView.fontFamily,
+        fontWeight: "bold",
+        color: "#fff",
+        fontSize: 18,
+        paddingBottom: 20,
+        textTransform: "uppercase",
+      },
       changableBody: {
         fontFamily: isEdit ? bodyEdit.fontFamily : bodyView.fontFamily,
         color: "#535353",
@@ -42,7 +74,7 @@ class Theme1About extends React.Component {
       },
       changableBody3: {
         fontFamily: isEdit ? bodyEdit.fontFamily : bodyView.fontFamily,
-        color: "#ffffff",
+        color: "#616E89",
         textAlign: "left",
         fontSize: 16,
       },
@@ -89,7 +121,7 @@ class Theme1About extends React.Component {
     const classes = useStyles();
 
     return (
-      <Grid container style={{ minHeight: "50vh", marginTop: 70 }}>
+      <Grid container justify={"center"} style={{ minHeight: "50vh", marginTop: 70 }}>
 
         <Grid item xs={12} style={{ marginBottom: 50 }}>
           <p style={classes.changableTitle}>
@@ -101,10 +133,11 @@ class Theme1About extends React.Component {
         </Grid>
 
         <Grid container item xs={12} justify={"center"} style={{ backgroundColor: isEdit ? titleEdit.color : titleView.color, minHeight: 420 }}>
-          <Grid item xs={12} sm={9}>
+          <Grid item xs={12} sm={12} style={{ marginLeft: "25%" }}>
             <img src={this.renderImage()} alt="" style={{ width: 292, height: 348, marginTop: -50, objectFit: "cover", display: isEdit ? (this.props.siteEdit.showDetailSetting.showAboutLogo ? "inline-block" : "none") : (this.props.siteView.showDetailSetting.showAboutLogo ? "block" : "none") }} />
 
-            <span style={{ height: 298, overflowY: "auto", maxWidth: 400, padding: 15, display: isEdit ? (this.props.siteEdit.showDetailSetting.showAboutDescription ? "inline-block" : "none") : (this.props.siteView.showDetailSetting.showAboutDescription ? "inline-block" : "none") }} >
+            <span style={{ height: 298, overflowY: "auto", maxWidth: 400, paddingLeft: 35, paddingTop: 30, display: isEdit ? (this.props.siteEdit.showDetailSetting.showAboutDescription ? "inline-block" : "none") : (this.props.siteView.showDetailSetting.showAboutDescription ? "inline-block" : "none") }} >
+              <p style={classes.changableTitle2}>Introduction</p>
               <p style={classes.changableBody3}>
                 {isEdit && siteEdit && siteEdit.about}
                 {!isEdit && siteView && siteView.about}
@@ -112,6 +145,29 @@ class Theme1About extends React.Component {
                 {!isEdit && !siteView.about && "Welcome to our website!"}
               </p>
             </span>
+          </Grid>
+        </Grid>
+
+        <Grid container item xs={12} justify={"center"} style={{ backgroundColor: isEdit ? lighten(titleEdit.color, 0.08) : lighten(titleView.color, 0.08), minHeight: 420, display: !fromHome ? "block" : "none", paddingTop: 50 }}>
+          <Grid item xs={12} sm={12} style={{ marginLeft: "25%" }}>
+            <span style={{ height: 298, overflowY: "auto", maxWidth: 400, paddingRight: 35, paddingTop: 30, display: "inline-block" }} >
+              <p style={classes.changableTitle2}>
+                {isEdit
+                  ? siteEdit && siteEdit.story && siteEdit.story.title
+                  : siteView && siteView.story && siteView.story.title}
+                {isEdit && !siteEdit.story && !siteEdit.story.title && "Your story title"}
+                {!isEdit && !siteView.story && !siteView.story.title && "Your story title"}
+              </p>
+              <p style={classes.changableBody3}>
+                {isEdit
+                  ? siteEdit && siteEdit.story && siteEdit.story.composedText
+                  : siteView && siteView.story && siteView.story.composedText}
+                {isEdit && !siteEdit.story && !siteEdit.story.composedText && "Create story on your Facebook page or inside Content Settings"}
+                {!isEdit && !siteView.story && !siteView.story.composedText && "Create story on your Facebook page or inside Content Settings"}
+              </p>
+            </span>
+
+            <img src={newCover && newCover[1] ? this.getCover(1) : this.getCover(0)} alt="" style={{ width: 292, height: 348, objectFit: "cover", display: "inline-block" }} />
           </Grid>
         </Grid>
 
@@ -131,6 +187,7 @@ const mapStateToProps = (state) => ({
   bodyEdit: state.site.bodyEdit,
   bodyView: state.site.bodyView,
   newLogo: state.site.newLogo,
+  newCover: state.site.newCover,
 });
 
 export default connect(mapStateToProps, null)(Theme1About);
